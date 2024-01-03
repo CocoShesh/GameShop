8c1fa69d67f6c992dcc206c69b154f8b7aff2ea8
NARD <110368170+CocoShesh@users.noreply.github.com>
2024-01-03 22:47:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "GameShop", // Replace 'your-repo-name' with your actual GitHub repository name
+  base: "/GameShop/", // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [react()],
   // Other configuration options...
 });
